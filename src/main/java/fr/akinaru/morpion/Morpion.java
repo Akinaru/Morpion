7e114fc11c4958bd1bfa91338e939f0e4bc88357
Akinaru <maxime.utchimata@gmail.com>
2022-10-22 13:29:30 +0200

    update

---
+++ src/main/java/fr/akinaru/morpion/Morpion.java
@@ -0,0 +1,19 @@
+package fr.akinaru.morpion;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Morpion extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Plugin de Morpion activé !");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
